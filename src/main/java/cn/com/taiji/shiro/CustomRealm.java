ab17859b9afbeaf8caf0cbecf0b460e339f39e33
冯理强 <fengliqiang@example.com>
2019-01-25 17:29:06 +0800

    domain和mapper完成

--- src/main/java/cn/com/taiji/shiro/CustomRealm.java
+++ src/main/java/cn/com/taiji/shiro/CustomRealm.java
@@ -1,4 +1,29 @@
 package cn.com.taiji.shiro;
 
-public class CustomRealm {
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.AuthenticationInfo;
+import org.apache.shiro.authc.AuthenticationToken;
+import org.apache.shiro.authz.AuthorizationInfo;
+import org.apache.shiro.realm.AuthorizingRealm;
+import org.apache.shiro.subject.PrincipalCollection;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class CustomRealm extends AuthorizingRealm {
+
+    Logger logger = LoggerFactory.getLogger(this.getClass());
+
+
+
+
+    @Override
+    protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principals) {
+        return null;
+    }
+
+    @Override
+    protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken token) throws AuthenticationException {
+        return null;
+    }
 }
